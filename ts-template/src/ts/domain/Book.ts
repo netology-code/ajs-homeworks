c989a596561fa1fc71ac069b2a14e55800d01f0a
aoovcharenko <61197587+aoovcharenko@users.noreply.github.com>
2022-12-27 18:38:52 +0300

    Update (Даниил Коротков)

--- ts-template/src/ts/domain/Book.ts
+++ ts-template/src/ts/domain/Book.ts
@@ -1,15 +1,5 @@
 import Buyable from './Buyable';
 
-/**                                                                                                                                                                                                                                                                                  
- * @module user                                                                                                                                                                                                                                                                      
- */                                                                                                                                                                                                                                                                                  
-                                                                                                                                                                                                                                                                                     
-/**                                                                                                                                                                                                                                                                                  
- * Load user info by id                                                                                                                                                                                                                                                              
- *                                                                                                                                                                                                                                                                                   
- * @param {number} id user id                                                                                                                                                                                                                                                        
- * @returns {Object} user info                                                                                                                                                                                                                                                       
- */
 export default class Book implements Buyable {
     constructor(
         readonly id: number,
