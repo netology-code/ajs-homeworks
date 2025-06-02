54bb30922a8f2697b61cd636b33623cdc2753eb2
Iuliia Aseeva <dragunovaulia39@gmail.com>
2025-06-02 17:10:30 +0300

    npm/babel

---
+++ workspace/defender-game/src/app.js
@@ -0,0 +1,8 @@
+const characters = [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'маг', health: 0},
+    {name: 'лучник', health: 0},
+  ];
+  
+  const alive = characters.filter(item => item.health > 0);
